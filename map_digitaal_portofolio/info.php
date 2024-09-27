cfcd03f9da82af79541fef051963ee11c6ba90d9
M100o000 <kevingemin31@gmail.com>
2024-09-27 17:08:44 +0200

    changes index php
    Extra link in html
    PHP text

--- map_digitaal_portofolio/info.php
+++ map_digitaal_portofolio/info.php
@@ -1,5 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <?php phpinfo(); ?> -->
 
-phpinfo();
+</head>
+<body>
+<?php
+$dev = ":)";
+echo 'hello darkness my friend';
+echo $dev;
+?><br>
+<?php 
+     
+
 
 ?>
+
+</body>
+</html>
+    
